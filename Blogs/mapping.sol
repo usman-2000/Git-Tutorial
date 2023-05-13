d75ec15821d9cdf09f46846c43134d36c5ea9aef
usmanrahim <usmanrahim2000@gmail.com>
2023-05-13 12:39:56 +0500

    Git and github tutorial

---
+++ Blogs/mapping.sol
@@ -0,0 +1,8 @@
+A data type that helps us to map keys with values.
+It is more likely to dictionary in the python language.
+
+mapping(uint=>string) public student;
+
+why we don't use arrays instead of mapping?
+
+because in arrays we need indexes to get or set a value. We can only set values in order. Like if a student has  a roll number of 1001, than in arrays we should assign empty spaces before 1001. But in mapping we simply assign the key and it returns tha value whenever need.
